1637aa1eca2938f955a89f62fa0ec9dce0982bda
yasha1212 <doenglish1@mail.ru>
2020-10-14 17:31:07 +0300

    Add unit tests for AssemblyParserLib

---
+++ AssemblyParserTest/MainTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyParserLib;
+using System.Collections.Generic;
+using AssemblyParserLib.TreeParts;
+using System.Linq;
+
+namespace AssemblyParserTest
+{
+    public static class CustomExtensions
+    {
+        public static void DoNothing(this MainTest instance, int x, string s) { }
+    }
+
+    public class GenericClass<T>
+    {
+        private T GetValueMethod(T data)
+        {
+            return data;
+        }
+    }
+
+    [TestClass]
+    public class MainTest
+    {
+        private int i;
+        private string s;
+        private AssemblyTree tree = (new AssemblyParser()).ParseFromFile(@"D:\University\GitRepos\mpp-5sem-3\AssemblyParserTest\bin\Debug\AssemblyParserTest.dll");
+
+        private List<T> GetListGenericMethod<T>()
+        {
+            return new List<T>();
+        }
+
+        [TestMethod]
+        public void TestAssemblyParameters()
+        {
+            Assert.AreEqual(1, tree.Namespaces.Count);
+            Assert.AreEqual("AssemblyParserTest", tree.Namespaces[0].Name);
+            Assert.AreEqual(4, tree.Namespaces[0].DataTypes.Count);
+        }
+
+        [TestMethod]
+        public void TestGenericClass()
+        {
+            Assert.AreEqual("GenericClass<T>", tree.Namespaces[0].DataTypes[1].Name);
+            Assert.AreEqual("T GetValueMethod(T)", tree.Namespaces[0].DataTypes[1].Methods[0].Signature);
+            Assert.AreEqual(0, tree.Namespaces[0].DataTypes[1].Fields.Count);
+            Assert.AreEqual(0, tree.Namespaces[0].DataTypes[1].Properties.Count);
+        }
+
+        [TestMethod]
+        public void TestMainClass()
+        {
+            Assert.AreEqual(3, tree.Namespaces[0].DataTypes[2].Fields.Count);
+            Assert.AreEqual(0, tree.Namespaces[0].DataTypes[2].Properties.Count);
+            Assert.AreEqual("MainTest", tree.Namespaces[0].DataTypes[2].Name);
+            Assert.AreEqual("List<T> GetListGenericMethod<T>()", tree.Namespaces[0].DataTypes[2].Methods[0].Signature);
+            Assert.AreEqual("Int32 i", tree.Namespaces[0].DataTypes[2].Fields[0].Signature);
+            Assert.AreEqual("String s", tree.Namespaces[0].DataTypes[2].Fields[1].Signature);
+            Assert.AreEqual("AssemblyTree tree", tree.Namespaces[0].DataTypes[2].Fields[2].Signature);
+        }
+
+        [TestMethod]
+        public void TestExtensionMethod()
+        {
+            Assert.AreEqual(1, tree.Namespaces[0].DataTypes[2].Methods.Where(m => m.Signature.Contains("- Extension")).ToList().Count);
+        }
+    }
+}
---
+++ AssemblyParserTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyParserTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyParserTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b06803c6-8ad3-472e-96a8-f6b231c555b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
