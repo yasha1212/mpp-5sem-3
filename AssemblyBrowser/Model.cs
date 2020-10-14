7e4cff1a8c12c9d3ee8f350c11e5caac52c95ce5
yasha1212 <doenglish1@mail.ru>
2020-10-14 16:34:15 +0300

    Add assembly browser WPF application

---
+++ AssemblyBrowser/Model.cs
@@ -0,0 +1,45 @@
+﻿using AssemblyParserLib.TreeParts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string fileName;
+        private ObservableCollection<AssemblyTree> assemblyTree;
+
+        public string FileName
+        {
+            get => fileName;
+            set
+            {
+                fileName = value;
+                OnPropertyChanged("FileName");
+            }
+        }
+
+        public ObservableCollection<AssemblyTree> AssemblyTree
+        {
+            get => assemblyTree;
+            set
+            {
+                assemblyTree = value;
+                OnPropertyChanged("AssemblyTree");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ AssemblyBrowser/ViewModel.cs
@@ -0,0 +1,73 @@
+﻿using AssemblyParserLib;
+using AssemblyParserLib.TreeParts;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AssemblyBrowser
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Model model;
+        private ICommand openFileCommand;
+
+        public ObservableCollection<AssemblyTree> AssemblyTree
+        {
+            get => model.AssemblyTree;
+            set
+            {
+                model.AssemblyTree = value;
+                OnPropertyChanged("AssemblyTree");
+            }
+        }
+
+        public string FileName
+        {
+            get => model.FileName;
+            set
+            {
+                model.FileName = value;
+
+                var parser = new AssemblyParser();
+                AssemblyTree = new ObservableCollection<AssemblyTree>
+                {
+                    parser.ParseFromFile(model.FileName)
+                };
+                OnPropertyChanged("FileName");
+            }
+        }
+
+        public ICommand OpenFileCommand
+        {
+            get
+            {
+                return openFileCommand ?? new OpenFileCommand(obj => 
+                {
+                    var openFileDialog = new OpenFileDialog();
+
+                    FileName = openFileDialog.ShowDialog() == true ? openFileDialog.FileName : FileName;
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel(Model model)
+        {
+            this.model = model;
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
